669fd51a1f6ff2bdab12ecf8af5b0f4ca5f2e70e
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2021-01-14 20:36:32 -0300

    Delete 019.py

--- desafios/019.py
+++
@@ -1,6 +0,0 @@
-from random import choice
-a1 = input("Digite o nome do primeiro aluno: ")
-a2 = input("Digite o nome do segundo aluno: ")
-a3 = input("Digite o nome do terceiro aluno: ")
-a4 = input("Digite o nome do quarto aluno: ")
-print("O sorteado é {}".format(choice([a1, a2, a3, a4])))
