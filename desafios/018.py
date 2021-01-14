6d86185a9dde45485f5251a42ac77ec5db263ce1
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2021-01-14 20:36:23 -0300

    Delete 018.py

--- desafios/018.py
+++
@@ -1,6 +0,0 @@
-import math
-
-num = float(input("Digite um angulo: "))
-print("Seno: {:.2f}".format(math.sin(math.radians(num))))
-print("Cosseno: {:.2f}".format(math.cos(math.radians(num))))
-print("Tangente: {:.2f}".format(math.tan(math.radians(num))))
