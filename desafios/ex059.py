c5905caaa5a6176a22cdf11f8b7c372a1850e5ef
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2021-03-13 21:25:17 -0300

    correções ex059

--- desafios/ex059.py
+++ desafios/ex059.py
@@ -12,17 +12,20 @@ while not escolha == 5:
                         "[5] Sair do programa\n"))
     if escolha == 1:
         print(f"A soma de {n1} com {n2} é de {n1+n2}\n")
-    if escolha == 2:
+        quit()
+    elif escolha == 2:
         print(f"A multiplicacao de {n1} com {n2} é de {n1*n2}\n")
-    if escolha == 3:
+        quit()
+    elif escolha == 3:
         if n1 > n2:
             maior = n1
         else:
             maior = n2
         print(f"O maior numero é {maior}\n")
-    if escolha == 4:
+        quit()
+    elif escolha == 4:
         continue
-    if escolha == 5:
+    elif escolha == 5:
         break
     else:
         print("Voce escolheu uma opcao incorreta. Tente novamenteo!\n")
